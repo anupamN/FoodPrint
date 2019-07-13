fcf0f91afea3d0f607d658673d40490814585ca1
Patrick Saw <Patrick.Saw@ing.com.au>
2019-07-13 15:34:58 +1000

    done some stuff

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -9,31 +9,53 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import './style.css';
+import meat from '../../images/icon-meat.png';
+import processed from '../../images/icon-processed.png';
+import freezer from '../../images/icon-freezer.png';
+import ausMade from '../../images/icon-AusMade.png';
+import plastic from '../../images/icon-containsPlastics.png';
+import star from '../../images/star.svg';
 
-const Product = ({ name = '', rating, isMeat, isProcessed, isFrozen, isPlasticPackaged }) => {
+const Product = ({ name = '', rating, isMeat, isProcessed, isFrozen, isPlasticPackaged, isLocal }) => {
+    const calculateRating = (isMeat, isProcessed, isFrozen, isPlasticPackaged, isLocal) => {
+      let rating = 5;
+      rating = isMeat ? rating-1 : rating;
+      rating = isProcessed ? rating-1 : rating;
+      rating = isFrozen ? rating-1 : rating;
+      rating = isPlasticPackaged ? rating-1 : rating;
+      rating = !isLocal ? rating-1 : rating;
+      console.log(rating);
+      return rating;
+    }
     return (
         <Card className="Product-wrapper">
             <CardContent>
                 <div className="Product-content">
                     <div className="Image-Content">
                         <div className="Product-image">
-                            <img alt="placeholder" src="http://placekitten.com/g/100/100" />
+                            <img className="product-image" alt="placeholder" src="http://placekitten.com/g/100/100" />
                         </div>
                     </div>
                     <div className="Product-Details">
                         <h1>{name}</h1>
+                        <div className="product-rating">
+                          {Array.from(new Array(calculateRating(isMeat, isProcessed, isFrozen, isLocal, isPlasticPackaged))).map((each, i) => (<img key={name + i} src={star}></img>))}
+                        </div>
                         <List className="Product-list">
-                            <ListItem>
-                                <ListItemText primary="Meat" />
-                                <ListItemIcon>{isMeat ? <Check /> : <Close />}</ListItemIcon>
+                            <ListItem hidden={!isMeat}>
+                                {isMeat ? (<img className="food-icon" alt="meat icon" src={meat}></img>) : <span></span>}
                             </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Processed" />
-                                <ListItemIcon>{isProcessed ? <Check /> : <Close />}</ListItemIcon>
+                            <ListItem hidden={!isProcessed}>
+                            {isProcessed ? (<img className="food-icon" alt="processed icon" src={processed}></img>) : <span></span>}
                             </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Frozen" />
-                                <ListItemIcon>{isFrozen ? <Check /> : <Close />}</ListItemIcon>
+                            <ListItem hidden={!isFrozen}>
+                            {isFrozen ? (<img className="food-icon" alt="meat icon" src={freezer}></img>) : <span></span>}
+                            </ListItem>
+                            <ListItem hidden={!isPlasticPackaged}>
+                            {isPlasticPackaged ? (<img className="food-icon" alt="plastic icon" src={plastic}></img>) : <span></span>}
+                            </ListItem>
+                            <ListItem hidden={!isLocal}>
+                            {isLocal ? (<img className="food-icon" alt="local icon" src={ausMade}></img>) : <span></span>}
                             </ListItem>
                         </List>
                     </div>
--- src/consts.js
+++ src/consts.js
@@ -1,67 +1,326 @@
 export const results = [
   {
-    name: 'Carrots',
-    category: ['vegetable'], 
-    isLocal: false,
-    isMeat: false,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
-  }, {
-    name: '',
-    category: [], 
-    isLocal: false,
-    isMeat: true,
-    isProcessed: true,
-    isFrozen: true,
-    isPlasticPackaged: false
+      name: "Carmans Muesli",
+      category: "pantry",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Barley Muesli",
+      category: "pantry",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "GingerBeer Saxbys",
+      category: "juices",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Breakfast juice",
+      category: "juices",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Fizi Natural water",
+      category: "juices",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Safcol tuna",
+      category: "seafood",
+      isLocal: false,
+      isMeat: true,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Random fish",
+      category: "seafood",
+      isLocal: false,
+      isMeat: true,
+      isProcessed: true,
+      isFrozen: true,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Sam's fish",
+      category: "seafood",
+      isLocal: true,
+      isMeat: true,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Jalna Organic yogurt",
+      category: "dairy",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "chuad fat free yogurt",
+      category: "dairy",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Basmati  white rice",
+      category: "rice",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Australian Brown rice",
+      category: "rice",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Bokashi Brown rice",
+      category: "rice",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Fiji Apples",
+      category: "fresh",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+   {
+      name: "organic fresh Apples loose",
+      category: "fresh",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Royal Gala Apples 1 kg",
+      category: "fresh",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Bourke street bread",
+      category: "bakery",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Bakers delight bread",
+      category: "bakery",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "sonoma bogette",
+      category: "bakery",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "keto bread",
+      category: "bakery",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Toscano Waffles",
+      category: "bakery",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Nanna's Waffles",
+      category: "bakery",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "White Wings Plain Flour 1kg",
+      category: "flour",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+   {
+      name: "White Wings Wholemeal Flour 1kg",
+      category: "flour",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: false,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "White Wings corn Flour 1kg",
+      category: "flour",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "President Camembert Cheese",
+      category: "cheese",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Mon Ami French Brie Cheese ",
+      category: "cheese",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Woolworths Triple Cream Brie ",
+      category: "cheese",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Pringles Chicken Salt Crips ",
+      category: "chips",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "Red Rock Deli Share Pack Sea Salt ",
+      category: "chips",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Arnott's Shapes Savoury ",
+      category: "chips",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: false
+  },
+  {
+      name: "AFantastic Rice Crackers ",
+      category: "chips",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Twisties Cheese Party Bag ",
+      category: "chips",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Burger Rings Party Bag ",
+      category: "chips",
+      isLocal: true,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Cc's Corn Chips Tasty Cheese ",
+      category: "chips",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
+  },
+  {
+      name: "Woolworths Corn Chips Supreme",
+      category: "chips",
+      isLocal: false,
+      isMeat: false,
+      isProcessed: true,
+      isFrozen: false,
+      isPlasticPackaged: true
   }
 ];

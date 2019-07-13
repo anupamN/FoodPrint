d71246da49e14cf3404a128fe4b831ba27716569
Ally Beaton <ally.beaton@lendi.com.au>
2019-07-13 14:52:29 +1000

    Added navbar, filtering of products

---
+++ src/foodDatabase.js
@@ -0,0 +1,110 @@
+export const mockData = [
+  { 
+    name: "Carmans Muesli",
+    category: "pantry",
+    isLocal: true,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: false
+  },
+  {
+    name: "Barley Muesli",
+    category: "pantry",
+    isLocal: true,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: false
+  },
+  {
+    name: "GingerBeer Saxbys",
+    category: "juices",
+    isLocal: true,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: false
+  },
+  {
+    name: "Breakfast juice",
+    category: "juices",
+    isLocal: false,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Fizi Natural water",
+    category: "juices",
+    isLocal: false,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Safcol tuna",
+    category: "seafood",
+    isLocal: false,
+    isMeat: true,
+    isProcessed: true,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Random fish",
+    category: "seafood",
+    isLocal: false,
+    isMeat: true,
+    isProcessed: true,
+    isFrozen: true,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Sam's fish",
+    category: "seafood",
+    isLocal: true,
+    isMeat: true,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: false
+  },
+  {
+    name: "Jalna Organic yogurt",
+    category: "dairy",
+    isLocal: true,
+    isMeat: false,             
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "chuad fat free yogurt",
+    category: "dairy",
+    isLocal: false,
+    isMeat: false,
+    isProcessed: true,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Basmati  white rice",
+    category: "rice",
+    isLocal: false,
+    isMeat: false,
+    isProcessed: true,
+    isFrozen: false,
+    isPlasticPackaged: true
+  },
+  {
+    name: "Australian Brown rice",
+    category: "rice",
+    isLocal: true,
+    isMeat: false,
+    isProcessed: false,
+    isFrozen: false,
+    isPlasticPackaged: true
+  }
+];

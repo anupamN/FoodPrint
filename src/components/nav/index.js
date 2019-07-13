6df42e54840c5607fcf3473bf7d8c6a64b801098
Ally Beaton <ally.beaton@lendi.com.au>
2019-07-13 15:19:36 +1000

    Added login to the nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './style.css';
 import logo from '../../logo.png';
+import Button from '@material-ui/core/Button';
 
 const Nav = () => (
-  <nav>
-    <img width="400px" src={logo} />
+  <nav className="Nav-wrapper">
+    <img alt="logo" width="400px" src={logo} />
+    <span className="Nav-right">
+      <Button>About</Button>
+      <Button variant="outlined" color="primary">
+        Login
+      </Button>
+    </span>
   </nav>
 );
 

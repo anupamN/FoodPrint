955da130dbc6313f3ac73fd8ea1729847b1ba9e5
Ally Beaton <ally.beaton@lendi.com.au>
2019-07-13 15:00:44 +1000

    Added about us page

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,7 +1,27 @@
 import React from 'react'; 
 
 export const About = () => (
-  <p>
-    test content
-  </p>
+  <>
+    <h1>About</h1>
+    <p>
+      Here at CarbonFOODprint we aim raise awareness and support of 
+      Food related carbon emissions from Australian communities and 
+      ask for the inclusion of a Carbon FOODprint Label on all foods.
+    </p>
+    <h2>What is CarbonFOODprint?</h2>
+    <p>Do you know how far your food has travelled to get to you? Have 
+      you seen the standards of factories where they came from? Do you 
+      wonder why we do not source more products locally?
+    </p>
+    <p>
+    You may not be aware, but where your food comes fromimpacts the world. 
+    You may not pay at the checkout, but you pay in excess fuel emissions 
+    from planes, from trash being dumped overseas, and with waste from 
+    animal grazing
+    </p>
+    <p>Our end goal is to hand over the petition and lobby to the government 
+      agencies; to the Minister of Environment & Energy and the Minister of 
+      Primary Industries (Food Standards Australia/NZ)
+    </p>
+  </>
 );
